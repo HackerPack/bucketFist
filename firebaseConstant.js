e322aa3c8f5bf1e5feaf6ef314ae33ae368d0fbf
rakesh <rsukuma2@ncsu.edu>
2016-02-21 00:36:15 -0600

    adding amadeus data component

---
+++ firebaseConstant.js
@@ -0,0 +1,12 @@
+var FIRE_BASE_URL = "https://bucketfist.firebaseio.com//";
+var WISH_TABLE = "wishes/";
+var USERS_TABLE = "users/";
+var ref = new Firebase(FIRE_BASE_URL);
+var isNewUser = true;
+var globalWishRef = new Firebase(FIRE_BASE_URL+WISH_TABLE);
+var globalUserRef = new Firebase(FIRE_BASE_URL+USERS_TABLE);
+var LIBRARY_ACCOUNT_ID = "56241a14de4bf40b17112a75";
+var DEBIT_ACCOUNT_ID = "56241a14de4bf40b17112a77";
+var ACCOUNT_URL = "http://api.reimaginebanking.com/accounts/"; 
+var TRANSFER_URL = "/transfers/"; 
+var CAPITAL_ONE_QUERY_PARAM = "?key=2ec3d395b0e81344514ca1ecbae6edcb";
